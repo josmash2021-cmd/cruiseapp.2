0fb61e02395e70b69b88cea63987d72155b7ef38
josmash2021-cmd <josmash2021@gmail.com>
2026-03-24 23:17:03 -0500

    fix: remove GooglePlaces native import - API used via Dart HTTP, not iOS SDK

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import GooglePlaces
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +7,6 @@ import GooglePlaces
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSPlacesClient.provideAPIKey("AIzaSyCf2Vya3o8KbubNuV0W_MdKEPMprXeisvo")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }

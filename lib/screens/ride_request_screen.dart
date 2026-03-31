1537e972ea57b70c1b596d14168614d2629604bb
josmash2021-cmd <josmash2021@gmail.com>
2026-03-31 06:09:16 -0500

    Fix floating pins: icon-pitch-alignment viewport->map so pins anchor flat to map surface

--- lib/screens/ride_request_screen.dart
+++ lib/screens/ride_request_screen.dart
@@ -474,7 +474,7 @@ class _RideRequestScreenState extends State<RideRequestScreen>
                     _pointAnnotMgr = await ctrl.annotations.createPointAnnotationManager();
                     try {
                       final lid = _pointAnnotMgr!.id;
-                      await ctrl.style.setStyleLayerProperty(lid, 'icon-pitch-alignment', 'viewport');
+                      await ctrl.style.setStyleLayerProperty(lid, 'icon-pitch-alignment', 'map');
                       await ctrl.style.setStyleLayerProperty(lid, 'icon-anchor', 'bottom');
                       await ctrl.style.setStyleLayerProperty(lid, 'icon-allow-overlap', true);
                       await ctrl.style.setStyleLayerProperty(lid, 'icon-ignore-placement', true);
